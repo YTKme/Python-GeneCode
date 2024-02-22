173a3c83933103f5692a4b812dc2cb399a6e4e3a
Yan Kuang <YTKme@Outlook.com>
2024-02-21 23:59:51 -0800

    Add conftest module for test configuration

---
+++ genecode/test/conftest.py
@@ -0,0 +1,46 @@
+"""
+Configure Test
+~~~~~~~~~~~~~~
+"""
+
+from pathlib import Path
+import pytest
+import tealogger
+
+from genecode import common
+
+
+# Configure Logger
+_conftest_logger = tealogger.Logger('conftest-logger')
+_conftest_logger.setLevel(tealogger.DEBUG)
+
+CURRENT_MODULE_PATH = Path(__file__).parent.expanduser().resolve()
+
+
+@pytest.fixture(scope='session')
+def email() -> str:
+    """Email
+
+    Get and return the `email` address.
+
+    :return: The `email` address
+    :rtype: str
+    """
+
+    data = common.load_json_file(CURRENT_MODULE_PATH.parent / 'metadata.json')
+
+    return data['database']['email']
+
+
+@pytest.fixture(scope='session')
+def setup_teardown_database():
+    """Setup and Teardown Database
+
+    Prepare and finalize test for `database` functionality.
+    """
+
+    # Execute before test
+
+    yield
+
+    # Execute after test
