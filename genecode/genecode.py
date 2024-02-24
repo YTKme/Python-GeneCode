701a3b24d4fd67ccfe6694dfc5b32a0f6f7f6bc6
Yan Kuang <YTKme@Outlook.com>
2024-02-24 08:01:43 -0800

    Format genecode module

--- genecode/genecode.py
+++ genecode/genecode.py
@@ -5,8 +5,10 @@ Gene Code
 Gene Code is a Python package and application for genetic development.
 """
 
+
 def main():
     print('Hello Gene Code Module!')
+
 
 if __name__ == '__main__':
     main()
