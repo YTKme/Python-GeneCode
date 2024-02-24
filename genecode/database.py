4b6fa0a0165190ab511ccdb8c4595e70c39c4b19
Yan Kuang <YTKme@Outlook.com>
2024-02-24 08:02:54 -0800

    Complete Version 1 of search function for Database class

--- genecode/database.py
+++ genecode/database.py
@@ -10,20 +10,20 @@ from Bio import SeqIO
 import tealogger
 
 from genecode.constant import (
-    RETRIEVE_MAXIMUM,
+    DEFAULT_RETMAX,
 )
 
 
 class Database:
     """Database"""
 
-    def __new__(
-        cls,
-        *args,
-        **kwargs
-    ) -> None:
-        """New"""
-        return super().__new__(cls)
+    # def __new__(
+    #     cls,
+    #     *args,
+    #     **kwargs
+    # ) -> None:
+    #     """New"""
+    #     return super().__new__(cls)
 
     def __init__(
         self,
@@ -50,82 +50,78 @@ class Database:
         database: str,
         term: str,
         **kwargs,
-    ) -> None:
+    ) -> list:
         """Search
 
         :param database: The `database` to search
         :type database: str
         :param term: The `term` (query) to search
         :type term: str
+        :return: The list of `IdList` from the search result
+        :rtype: list
         """
 
         # Configure the Entrez email
         Entrez.email = self._email
         Entrez.api_key = self._api_key
 
-        # Execute the search and get a `handle` for the result(s)
-        handle = Entrez.esearch(
-            db=database,
-            term=term,
-            **kwargs,
-        )
+        try:
+            # Execute the search and get a `handle` for the result(s)
+            handle = Entrez.esearch(
+                db=database,
+                term=term,
+                **kwargs,
+            )
 
-        # Execute an initial `read` to parse the result(s)
-        record = Entrez.read(handle)
-        # Get a `Count` of the result(s), convert to `int`
-        record_count = int(record['Count'])
-        self._logger.debug('Record Count: %s', record_count)
-        self._logger.debug(f'Record: {record}')
+            # Execute an initial `read` to parse the result(s)
+            record = Entrez.read(handle)
+            # Get the `Count` of the result(s), convert to `int`
+            record_count = int(record['Count'])
+            self._logger.debug('Record Count: %s', record_count)
+            # Get the `RetMax` of the result(s), convert to `int`
+            record_retmax = int(record['RetMax'])
+            self._logger.debug('Record RetMax: %s', record_retmax)
 
-        # if (kwargs.get('retmax') and kwargs.get('retmax') > record_count) or (constant.RETRIEVE_MAX > record_count):
-        #     ...
+            # self._logger.debug(f'Record: {record}')
 
-    async def _query(
+            # Parse the `IdList` as current `result_list`
+            result_list = record['IdList']
+
+            # Compare record `Count` (total) to `RetMax` (retrieved)
+            if record_retmax < record_count:
+                # If the `RetMax` is less than the `Count`
+                record_remain = record_count - record_retmax
+                self._logger.debug('Record Remain: %s', record_remain)
+
+                # Update the `retmax` with the remaining record(s)
+                # - Use as `kwargs` for the next retrieve
+                # - Account for previous `kwargs` function argument
+                kwargs['retmax'] = record_remain
+
+                # Need to retrieve more record(s)
+                handle = Entrez.esearch(
+                    db=database,
+                    term=term,
+                    **kwargs,
+                )
+                record = Entrez.read(handle)
+
+                # Combine the new `IdList` with current `result_list`
+                result_list += record['IdList']
+
+            # Return the list of result(s)
+            self._logger.debug('Result List: %s', result_list)
+            return result_list
+        except Exception as e:
+            # TODO:
+            pass
+        finally:
+            # Clean up by flush and close the `handle` stream
+            handle.close()
+
+
+    async def download(
         self,
-        database: str,
-        term: str,
-        retrieve_start: int = 0,
-        retrieve_max: int = RETRIEVE_MAXIMUM,
-    ) -> list[str]:
-        """Query
-
-        :param database: The `database` to query
-        :type database: str
-        :param term: The `term` (query) to search
-        :type term: str
-        :param retrieve_start: The sequential index of the first UID in
-            the retrieved set in output, defaults to `0`
-        :type retrieve_start: int, optional
-        :param retrieve_max: The total number of UIDs from the retrieved
-            set in output, defaults to `RETRIEVE_MAX`
-        :type retrieve_max: int, optional
-        :return: The list of UID(s)
-        :rtype: list[str]
-        """
-        # Configure the Entrez email
-        Entrez.email = self._email
-
-        # Execute the search and get a `handle` for the result(s)
-        handle = Entrez.esearch(
-            db=database,
-            term=term,
-            retstart=retrieve_start,
-            retmax=retrieve_max,
-        )
-
-        # Execute an initial `read` to parse the result(s)
-        record = Entrez.read(handle)
-
-        return record['IdList']
-
-
-# query = '"escherichia coli"[Organism]'
-
-# Entrez.email = 'ytkme@outlook.com'
-# handle = Entrez.esearch(db='Nucleotide', term=query, retmax=200, retstart=201)
-# record = Entrez.read(handle)
-# handle.close()
-
-# print(f'Record Count: {record["Count"]}')
-
-# print(f'Data: {record["IdList"]}')
+    ) -> None:
+        """Download"""
+        pass
